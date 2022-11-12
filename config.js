74420d61c7bae466f74ee0a4a810d207c9bfc607
ramss-md <117951066+ramss-md@users.noreply.github.com>
2022-11-12 18:10:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,11 +16,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['62895604670507']
-global.ownernomer = "62895604670507"
-global.premium = ['62895604670507','6281328144300']
-global.packname = 'Zenss'
-global.author = 'Cuyy'
+global.owner = ['6285715352771']
+global.ownernomer = "6285715352771"
+global.premium = ['6285715352771','6285715352771']
+global.packname = 'Ramss'
+global.author = 'ramss'
 global.sessionName = 'zenss'
 global.prefa = ['','!','.','#','&']
 global.sp = ''
